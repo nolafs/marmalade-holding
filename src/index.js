40c123322e98ed5da29935d53b885c29e935b56d
nolafs <Sontag2010>
2019-12-20 10:48:13 +0000

    add faq cms

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ import 'foundation-sites/dist/js/plugins/foundation.toggler.min';
 //import 'foundation-sites/dist/js/plugins/foundation.drilldown.min';
 //import 'foundation-sites/dist/js/plugins/foundation.equalizer.min';
 //import 'foundation-sites/dist/js/plugins/foundation.responsiveAccordionTabs.min';
+
+import 'foundation-sites/dist/js/plugins/foundation.responsiveAccordionTabs.min';
+import 'foundation-sites/dist/js/plugins/foundation.accordion.min';
 //import 'foundation-sites/dist/js/plugins/foundation.smoothScroll.min';
 //import 'foundation-sites/dist/js/plugins/foundation.dropdown.min';
 import 'foundation-sites/dist/js/plugins/foundation.interchange.min';
